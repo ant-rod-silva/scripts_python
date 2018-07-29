51ad8a16871dc35ee1a05110265b7b85c5462481
Antonio Rodrigo dos Santos Silva <20875150+rodrigosantosbr@users.noreply.github.com>
2018-07-29 10:59:06 -0300

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# Hello World
+# By Antonio Rodrigo dos Santos Silva
+# Python 3
+
+print("Welcome!!!")
+print("This is my first python 3 app")
+name = input("What is your name? ")
+surname = input("What is your surname? ")
+print("How are you, {}{}?".format(name,surname))
